73bfc33b2d9581e09b2b7fa3f8c30173450d08b3
Colin Gidzinski <evg31337@gmail.com>
2016-09-17 23:05:15 -0400

    root fix

--- routes/api/root.js
+++ routes/api/root.js
@@ -69,10 +69,8 @@ DB.Memory.find({}, function(err, memory) {
    app.post('/api/memory',isLoggedIn, function(req, res) {
     
 DB.User.findOne({_id:req.user._id}, function(err, user) {
-console.log(req.user._id);
-console.log(user.public.name);
 var memory = new DB.Memory();
-    memory.author = "Colin";
+    memory.author = user.public.name;
     memory.lat = req.body.lat;
     memory.lon = req.body.lon;
     memory.data = req.body.data;
