99b57f7dcb78590050b727ed1cc7e7c2239e2bae
Colin Gidzinski <evg31337@gmail.com>
2016-09-17 22:49:30 -0400

    fixed id

--- routes/api/root.js
+++ routes/api/root.js
@@ -67,7 +67,7 @@ DB.Memory.find({}, function(err, memory) {
 });
 
    app.post('/api/memory',isLoggedIn, function(req, res) {
-DB.User.findOne({'_id': _id}, function(err, user) {
+DB.User.findById({req.user._id}, function(err, user) {
 
 var memory = new DB.Memory();
     memory.author = user.public.name;
